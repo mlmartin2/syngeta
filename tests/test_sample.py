95863a482f7773cfa924ec76abfea77fb528b2e1
mlmartin2 <martinm@usp.br>
2022-05-11 17:19:49 -0300

    Testes preliminares p/ formatos de data

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -15,18 +15,18 @@ class MyTest(TestCase):
         result = "Ridgewood"
         self.assertEqual(result, get_cheapest_hotel("Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)"))
     
-    def test6(self):
+    def test5(self):
         result = [1, [0,1,2]]
         self.assertEqual(result, decode_defaultArgument("Rewards: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)"))
 
-    def test7(self):
+    def test6(self):
         result = [1, [2,3,4]]
         self.assertEqual(result, decode_defaultArgument("Rewards: 16Mar2009(wed), 17Mar2009(thur), 18Mar2009(fri)"))
 
-    def test8(self):
+    def test7(self):
         result = 0
         self.assertEqual(result, get_weekdayValue_fromString('(mon)'))
 
-    def test9(self):
+    def test8(self):
         result = 6
         self.assertEqual(result, get_weekdayValue_fromString('(sun)'))
